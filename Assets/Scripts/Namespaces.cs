2d35430e00f189b99077dfd3c4db87af9699524f
Victor Arnaud <victorhad@gmail.com>
2022-12-15 21:21:16 -0300

    Ajustanto formatação

--- Assets/Scripts/Namespaces.cs
+++ Assets/Scripts/Namespaces.cs
@@ -1,20 +1,16 @@
 using System;
 
-namespace Mammal
-{
-
+namespace Mammal {
     // interface -> Define quais operações o objeto que herdou irá implementar, diferente
     // do abstract vc não define nada dentro da classe apenas a casca.
-    interface AnimalInterface
-    {
+    interface AnimalInterface {
         public string Roar();
         public string Roar(string noise);
     }
 
     // sealed -> Faz com que a classe não possa ser herdada.
     // abstract -> Faz com que a classe não possa ser instanciada.
-    abstract class Animal : AnimalInterface
-    {
+    abstract class Animal : AnimalInterface {
         protected string type = "Carnivoro";
         public static string noise = "RAWWWW...";
         
@@ -22,81 +18,64 @@ namespace Mammal
         public abstract string Atk(string enemy);
 
         // Suas classes filhas podem sobrescrever o comportamento desse metodo. (Polimorfismo)
-        public virtual string Roar()
-        {
+        public virtual string Roar() {
             return noise;
         }
 
         // Sobrecarga de método (Mesmo nome do metodo mas estrutura diferente)
-        public string Roar(string noise)
-        {
+        public string Roar(string noise) {
             return noise;
         }
     }
 
-    class Feline : Animal
-    {
+    class Feline : Animal {
         public string name;
         private int strength;
 
         // Construtor (Destrutor ~Feline(){...} )
-        public Feline(string name, string type = null, int strength = 10)
-        {
+        public Feline(string name, string type = null, int strength = 10) {
             this.name = name;
             this.strength = strength;
 
-            if (type != null)
-            {
+            if (type != null) {
                 base.type = type;
             }
         }
 
         // Encapsulamento (GET e SET)
-        public int Strength
-        {
-            get
-            {
+        public int Strength {
+            get {
                 return strength;
             }
 
-            set
-            {
-                if (value >= 0 && value <= 100)
-                {
+            set {
+                if (value >= 0 && value <= 100) {
                     strength = value;
-                }
-                else
-                {
+                } else {
                     throw new Exception("Valor de força deve ser maior que zero e menor que 100.");
                 }
             }
         }
 
         // Polimorfismo (Sobrescrevendo o comportamento da classe pai)
-        public override string Roar()
-        {
+        public override string Roar() {
             return "Meow!!";
         }
 
         // Sobrescrita de classe abstrata
-        public override string Atk(string enemy)
-        {
+        public override string Atk(string enemy) {
             return $"Ataquei com força {strength} no inimigo {enemy}";
         }
 
-        private bool IsCarnivore()
-        {
-            if (type == "Carnivoro")
-            {
+        private bool IsCarnivore() {
+            if (type == "Carnivoro") {
                 return true;
             }
             return false;
         }
 
-        public string Eat()
-        {
-            if (IsCarnivore())
-            {
+        public string Eat() {
+            if (IsCarnivore()) {
                 return $"Comi carne";
             }
 
